df65146c13ccc9050af177210f9bae06ad4e56ff
hykw <hykw1234@gmail.com>
2014-07-28 17:54:02 +0900

    first version

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 hykw-wp-filecache
-=================
+====================
+
+# 動作
+## 指定ファイル名のキャッシュを確認
+- あれば単に返す
+- 無い、あるいはタイムスタンプが指定間隔よりも古い場合は上書き作成(未指定は24時間）
+
+    Usage:
+      $objCache = new hykwFileCache($dir_cache_custom_href);
+
+      $cacheFileName = sha1($content);
+      $ret = $objCache->readData($cacheFileName);
+      if ($ret != FALSE)
+        return $ret;
+                 ・
+                 ・
+                 ・
+                 ・
+      $objCache->writeData($cacheFileName, $content);
+
---
+++ main.php
@@ -0,0 +1,58 @@
+<?php
+  /**
+   * @package HYKW file cache plugin
+   * @version 0.1
+   */
+  /*
+    Plugin Name: HYKW file cache plugin
+    Plugin URI: https://github.com/hykw/hykw-wp-filecache
+    Description: ファイルキャッシュプラグイン
+    Author: hitoshi-hayakawa
+    Version: 0.1
+  */
+
+class hykwFileCache
+{
+  private $cachedir;
+  private $obs_min;
+
+  # キャッシュディレクトリ, キャッシュの寿命（分単位）
+  # e.g.
+  #   ('/tmp/cache', 1440)
+  function __construct($cachedir, $cache_obs_min = 1440)
+  {
+    $this->cachedir = $cachedir;
+    $this->obs_min = $cache_obs_min;
+
+    # ディレクトリが無かったら作っとく
+    if (!file_exists($cachedir))
+      mkdir($cachedir, 0755);
+  }
+
+  # not found/obsolete の場合、FALSEを返す
+  function readData($filename)
+  {
+    $cache_dirFile = sprintf('%s/%s', $this->cachedir, $filename);
+    if (!file_exists($cache_dirFile))
+      return FALSE;
+
+    $ctime = filectime($cache_dirFile);
+    if ( ($ctime + $this->obs_min*60) < time() ) {
+      # キャッシュは無効
+      unlink($cache_dirFile);
+      return FALSE;
+    }
+
+    return file_get_contents($cache_dirFile);
+  }
+  
+
+  function writeData($filename, $content)
+  {
+    $cache_dirFile = sprintf('%s/%s', $this->cachedir, $filename);
+    $fp = fopen($cache_dirFile, 'w');
+    fputs($fp, $content);
+    fclose($fp);
+  }
+
+}
